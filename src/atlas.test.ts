a2c144be331cdf4b524fd603cad0cbdc1e2f62b6
tiagohm <tiago.henrique.cco@gmail.com>
2025-02-01 10:17:10 -0300

    implement atlas

--- src/atlas.test.ts
+++ src/atlas.test.ts
@@ -1,9 +1,101 @@
-import { test } from 'bun:test'
-import { atlas } from './atlas'
+import { expect, test } from 'bun:test'
+import { type BodyPosition, atlas } from './atlas'
 
 test('positionOfSun', async () => {
-	const uri = encodeURI('http://localhost/atlas/sun/position?dateTime=2025-01-31T21:36:00&longitude=-45&latitude=-23&elevation=890&offsetInMinutes=-180')
-	const request = new Request(uri)
+	const request = new Request('http://localhost/atlas/sun/position?dateTime=2025-01-31T21:36:00&longitude=-45&latitude=-23&elevation=890')
 	const response = await atlas.handle(request)
-	console.log(await response.text())
+	const position = (await response.json()) as BodyPosition
+
+	expect(position.rightAscensionJ2000).toBeCloseTo(5.487010782848625, 12)
+	expect(position.declinationJ2000).toBeCloseTo(-0.3004046745898624, 12)
+	expect(position.rightAscension).toBeCloseTo(5.498210909724522, 12)
+	expect(position.declination).toBeCloseTo(-0.30089179598409405, 12)
+	expect(position.azimuth).toBeCloseTo(4.399644478917377, 12)
+	expect(position.altitude).toBeCloseTo(0.03410675300357515, 12)
+	expect(position.magnitude).toBeCloseTo(-26.774, 3)
+	expect(position.constellation).toBe('CAP')
+	expect(position.distance).toBeCloseTo(147408018.51227665, 6)
+	expect(position.distanceUnit).toBe('km')
+	expect(position.illuminated).toBe(100)
+	expect(position.elongation).toBe(0)
+	expect(position.leading).toBeFalse()
+})
+
+test('positionOfMoon', async () => {
+	const request = new Request('http://localhost/atlas/moon/position?dateTime=2025-01-15T21:36:00&longitude=-45&latitude=-23&elevation=890')
+	const response = await atlas.handle(request)
+	const position = (await response.json()) as BodyPosition
+
+	expect(position.rightAscensionJ2000).toBeCloseTo(2.510279836007113, 12)
+	expect(position.declinationJ2000).toBeCloseTo(0.31919436571805776, 12)
+	expect(position.rightAscension).toBeCloseTo(2.5051473462757734, 12)
+	expect(position.declination).toBeCloseTo(0.31378681209660375, 12)
+	expect(position.azimuth).toBeCloseTo(1.3910762481519514, 12)
+	expect(position.altitude).toBeCloseTo(-0.4168798679935272, 12)
+	expect(position.magnitude).toBeCloseTo(-12.062, 3)
+	expect(position.constellation).toBe('LEO')
+	expect(position.distance).toBeCloseTo(392259.4844096544, 6)
+	expect(position.distanceUnit).toBe('km')
+	expect(position.illuminated).toBeCloseTo(95.41058, 4)
+	expect(position.elongation).toBeCloseTo(2.7085851928162596, 12)
+	expect(position.leading).toBeTrue()
+})
+
+test('positionOfMars', async () => {
+	const request = new Request('http://localhost/atlas/planets/499/position?dateTime=2025-01-31T21:36:00&longitude=-45&latitude=-23&elevation=890')
+	const response = await atlas.handle(request)
+	const position = (await response.json()) as BodyPosition
+
+	expect(position.rightAscensionJ2000).toBeCloseTo(1.9632543785239203, 12)
+	expect(position.declinationJ2000).toBeCloseTo(0.45620236293376226, 12)
+	expect(position.rightAscension).toBeCloseTo(1.9688629940752043, 12)
+	expect(position.declination).toBeCloseTo(0.45456541862831673, 12)
+	expect(position.azimuth).toBeCloseTo(0.9525893087384896, 12)
+	expect(position.altitude).toBeCloseTo(0.2125455679893387, 12)
+	expect(position.magnitude).toBeCloseTo(-1.144, 3)
+	expect(position.constellation).toBe('GEM')
+	expect(position.distance).toBeCloseTo(102231147.20948215, 6)
+	expect(position.distanceUnit).toBe('km')
+	expect(position.illuminated).toBeCloseTo(98.69578, 4)
+	expect(position.elongation).toBeCloseTo(2.75473169823899, 12)
+	expect(position.leading).toBeFalse()
+})
+
+test('altitudePointsOfSun', async () => {
+	let request = new Request('http://localhost/atlas/sun/position?dateTime=2025-01-31T21:36:00&longitude=-45&latitude=-23&elevation=890')
+	await atlas.handle(request)
+
+	request = new Request('http://localhost/atlas/sun/altitude-points?dateTime=2025-01-31T21:36:00&stepSize=5')
+	const response = await atlas.handle(request)
+	const points = (await response.json()) as number[]
+
+	expect(points).toHaveLength(289)
+	expect(points[0]).toBeCloseTo(0.7755328294268905, 12)
+	expect(points[288]).toBeCloseTo(0.7736406830782184, 12)
+})
+
+test('altitudePointsOfMoon', async () => {
+	let request = new Request('http://localhost/atlas/moon/position?dateTime=2025-01-15T21:36:00&longitude=-45&latitude=-23&elevation=890')
+	await atlas.handle(request)
+
+	request = new Request('http://localhost/atlas/moon/altitude-points?dateTime=2025-01-15T21:36:00&stepSize=5')
+	const response = await atlas.handle(request)
+	const points = (await response.json()) as number[]
+
+	expect(points).toHaveLength(289)
+	expect(points[0]).toBeCloseTo(-0.47675490641964197, 12)
+	expect(points[288]).toBeCloseTo(-0.27032654292561814, 12)
+})
+
+test('altitudePointsOfMars', async () => {
+	let request = new Request('http://localhost/atlas/planets/499/position?dateTime=2025-01-31T21:36:00&longitude=-45&latitude=-23&elevation=890')
+	await atlas.handle(request)
+
+	request = new Request('http://localhost/atlas/planets/499/altitude-points?dateTime=2025-01-31T21:36:00&stepSize=5')
+	const response = await atlas.handle(request)
+	const points = (await response.json()) as number[]
+
+	expect(points).toHaveLength(289)
+	expect(points[0]).toBeCloseTo(-1.127593682275471, 12)
+	expect(points[288]).toBeCloseTo(-1.1481425780232144, 12)
 })
--- src/atlas.ts
+++ src/atlas.ts
@@ -1,69 +1,111 @@
 import { Elysia, type Static, t } from 'elysia'
-import { type Angle, deg } from 'nebulosa/src/angle'
+import { type Angle, deg, parseAngle } from 'nebulosa/src/angle'
+import { AU_KM, SPEED_OF_LIGHT } from 'nebulosa/src/constants'
 import { type Distance, meter } from 'nebulosa/src/distance'
 import { type ObserverTable, Quantity, observer } from 'nebulosa/src/horizons'
 
-const LocationQuery = t.Object({
-	longitude: t.Number(),
-	latitude: t.Number(),
-	elevation: t.Number(),
-	offsetInMinutes: t.Number(),
-})
+const LOCATION_QUERY = t.Object({ longitude: t.Number({ minimum: -180, maximum: 180 }), latitude: t.Number({ minimum: -90, maximum: 90 }), elevation: t.Number() })
 
-export type Location = Readonly<Static<typeof LocationQuery>>
+const DATE_TIME_QUERY = t.Object({ dateTime: t.String() })
 
-const DateTimeQuery = t.Object({
-	dateTime: t.String(),
-})
+const POSITION_OF_BODY_QUERY = t.Composite([DATE_TIME_QUERY, LOCATION_QUERY])
 
-const PositionOfSunQuery = t.Composite([DateTimeQuery, LocationQuery])
+const ALTITUDE_POINTS_OF_BODY_QUERY = t.Composite([DATE_TIME_QUERY, t.Object({ stepSize: t.Number({ minimum: 1, maximum: 60 }) })])
 
-export type PositionOfSun = Readonly<Static<typeof PositionOfSunQuery>>
+const BODY_POSITIONS = new Map<string, Map<number, Readonly<BodyPosition>>>()
+
+const HORIZONS_QUANTITIES: Quantity[] = [Quantity.ASTROMETRIC_RA_DEC, Quantity.APPARENT_RA_DEC, Quantity.APPARENT_AZ_EL, Quantity.VISUAL_MAG_SURFACE_BRGHT, Quantity.ONE_WAY_DOWN_LEG_LIGHT_TIME, Quantity.ILLUMINATED_FRACTION, Quantity.SUN_OBSERVER_TARGET_ELONG_ANGLE, Quantity.CONSTELLATION_ID]
+
+export interface BodyPosition {
+	rightAscensionJ2000: number
+	declinationJ2000: number
+	rightAscension: number
+	declination: number
+	azimuth: number
+	altitude: number
+	magnitude: number
+	constellation: string
+	distance: number
+	distanceUnit: string
+	illuminated: number
+	elongation: number
+	leading: boolean
+}
+
+export type Location = Readonly<Static<typeof LOCATION_QUERY>>
+
+export type PositionOfSun = Readonly<Static<typeof POSITION_OF_BODY_QUERY>>
+
+export type PositionOfMoon = Readonly<Static<typeof POSITION_OF_BODY_QUERY>>
+
+export type PositionOfPlanet = Readonly<Static<typeof POSITION_OF_BODY_QUERY>>
+
+export type PositionOfSkyObject = Readonly<Static<typeof POSITION_OF_BODY_QUERY>>
+
+export type PositionOfSatellite = Readonly<Static<typeof POSITION_OF_BODY_QUERY>>
+
+export type AltitudePointsOfSun = Readonly<Static<typeof ALTITUDE_POINTS_OF_BODY_QUERY>>
+
+export type AltitudePointsOfMoon = Readonly<Static<typeof ALTITUDE_POINTS_OF_BODY_QUERY>>
+
+export type AltitudePointsOfPlanet = Readonly<Static<typeof ALTITUDE_POINTS_OF_BODY_QUERY>>
+
+export type AltitudePointsOfSkyObject = Readonly<Static<typeof ALTITUDE_POINTS_OF_BODY_QUERY>>
+
+export type AltitudePointsOfSatellite = Readonly<Static<typeof ALTITUDE_POINTS_OF_BODY_QUERY>>
 
 export const atlas = new Elysia({ prefix: '/atlas' })
 	.get('/sun/image', () => imageOfSun())
-	.get('/sun/position', ({ query }) => positionOfSun(query), { query: PositionOfSunQuery })
-	.get('/sun/altitude-points', () => altitudePointsOfSun())
+	.get('/sun/position', ({ query }) => positionOfSun(query), { query: POSITION_OF_BODY_QUERY })
+	.get('/sun/altitude-points', ({ query }) => altitudePointsOfSun(query), { query: ALTITUDE_POINTS_OF_BODY_QUERY })
 	.get('/earth/seasons', () => earthSeasons())
-	.get('/moon/position', () => positionOfMoon())
-	.get('/moon/altitude-points', () => altitudePointsOfMoon())
+	.get('/moon/position', ({ query }) => positionOfMoon(query), { query: POSITION_OF_BODY_QUERY })
+	.get('/moon/altitude-points', ({ query }) => altitudePointsOfMoon(query), { query: ALTITUDE_POINTS_OF_BODY_QUERY })
 	.get('/moon/phases', () => moonPhases())
 	.get('/twilight', () => twilight())
-	.get('/planets/{code}/position', () => positionOfPlanet())
-	.get('/planets/{code}/altitude-points', () => altitudePointsOfPlanet())
+	.get('/planets/:code/position', ({ query, params: { code } }) => positionOfPlanet(code, query), { query: POSITION_OF_BODY_QUERY })
+	.get('/planets/:code/altitude-points', ({ query, params: { code } }) => altitudePointsOfPlanet(code, query), { query: ALTITUDE_POINTS_OF_BODY_QUERY })
 	.get('/minor-planets', () => searchMinorPlanet())
 	.get('/minor-planets/close-approaches', () => closeApproachesForMinorPlanets())
 	.get('/sky-objects', () => searchSkyObject())
 	.get('/sky-objects/types', () => skyObjectTypes())
-	.get('/sky-objects/{id}/position', () => positionOfSkyObject())
-	.get('/sky-objects/{id}/altitude-points', () => altitudePointsOfSkyObject())
+	.get('/sky-objects/:id/position', ({ query, params: { id } }) => positionOfSkyObject())
+	.get('/sky-objects/:id/altitude-points', ({ query }) => altitudePointsOfSkyObject(query), { query: ALTITUDE_POINTS_OF_BODY_QUERY })
 	.get('/satellites', () => searchSatellites())
-	.get('/satellites/{id}/position', () => positionOfSatellite())
-	.get('/satellites/{id}/altitude-points', () => altitudePointsOfSatellite())
+	.get('/satellites/:id/position', ({ query, params: { id } }) => positionOfSatellite())
+	.get('/satellites/:id/altitude-points', ({ query }) => altitudePointsOfSatellite(query), { query: ALTITUDE_POINTS_OF_BODY_QUERY })
 
 function imageOfSun() {}
 
-const HORIZONS_QUANTITIES: Quantity[] = [Quantity.ASTROMETRIC_RA_DEC, Quantity.APPARENT_RA_DEC, Quantity.APPARENT_AZ_EL, Quantity.VISUAL_MAG_SURFACE_BRGHT, Quantity.ONE_WAY_DOWN_LEG_LIGHT_TIME, Quantity.ILLUMINATED_FRACTION, Quantity.SUN_OBSERVER_TARGET_ELONG_ANGLE, Quantity.CONSTELLATION_ID]
-
-async function positionOfSun(q: PositionOfSun) {
-	await computeEphemeris('10', new Date(q.dateTime), deg(q.longitude), deg(q.latitude), meter(q.elevation), q.offsetInMinutes)
+function positionOfSun(q: PositionOfSun) {
+	return computeEphemeris('10', new Date(q.dateTime), deg(q.longitude), deg(q.latitude), meter(q.elevation))
 }
 
-function altitudePointsOfSun() {}
+function altitudePointsOfSun(q: AltitudePointsOfSun) {
+	return computeAltitudePoints('10', new Date(q.dateTime), q.stepSize)
+}
 
 function earthSeasons() {}
 
-function positionOfMoon() {}
+function positionOfMoon(q: PositionOfMoon) {
+	return computeEphemeris('301', new Date(q.dateTime), deg(q.longitude), deg(q.latitude), meter(q.elevation))
+}
 
-function altitudePointsOfMoon() {}
+function altitudePointsOfMoon(q: AltitudePointsOfMoon) {
+	return computeAltitudePoints('301', new Date(q.dateTime), q.stepSize)
+}
 
 function moonPhases() {}
 
 function twilight() {}
 
-function positionOfPlanet() {}
+function positionOfPlanet(code: string, q: PositionOfPlanet) {
+	return computeEphemeris(code, new Date(q.dateTime), deg(q.longitude), deg(q.latitude), meter(q.elevation))
+}
 
-function altitudePointsOfPlanet() {}
+function altitudePointsOfPlanet(code: string, q: AltitudePointsOfPlanet) {
+	return computeAltitudePoints(code, new Date(q.dateTime), q.stepSize)
+}
 
 function searchMinorPlanet() {}
 
@@ -75,21 +117,81 @@ function skyObjectTypes() {}
 
 function positionOfSkyObject() {}
 
-function altitudePointsOfSkyObject() {}
+function altitudePointsOfSkyObject(q: AltitudePointsOfSkyObject) {}
 
 function searchSatellites() {}
 
 function positionOfSatellite() {}
 
-function altitudePointsOfSatellite() {}
+function altitudePointsOfSatellite(q: AltitudePointsOfSatellite) {}
 
-async function computeEphemeris(command: string, dateTime: Date, longitude: number, latitude: Angle, elevation: Distance, offsetInMinutes: number) {
-	const startTime = new Date(dateTime.getTime() + offsetInMinutes * 60 * 1000)
+async function computeEphemeris(code: string, date: Date, longitude: number, latitude: Angle, elevation: Distance) {
+	const time = Math.trunc(date.getTime() / 1000)
+
+	const position = BODY_POSITIONS.get(code)?.get(time)
+	if (position) return position
+
+	let startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0, 0)
+	if (date.getHours() < 12) startTime = new Date(startTime.getTime() - 86400000)
 	const endTime = new Date(startTime.getTime() + 86400000)
-	const ephemeris = await observer(command, 'coord', [longitude, latitude, elevation], startTime, endTime, HORIZONS_QUANTITIES)
-	makeBodyPositionFromEphemeris(ephemeris!)
+
+	const ephemeris = await observer(code, 'coord', [longitude, latitude, elevation], startTime, endTime, HORIZONS_QUANTITIES)
+	const positions = makeBodyPositionFromEphemeris(ephemeris!)
+	if (!BODY_POSITIONS.has(code)) BODY_POSITIONS.set(code, new Map())
+	const map = BODY_POSITIONS.get(code)!
+	positions.forEach((e) => map.set(e[0], e[1]))
+	return map.get(time)
 }
 
-function makeBodyPositionFromEphemeris(ephemeris: ObserverTable) {
-	//
+function computeAltitudePoints(code: string, date: Date, stepSizeInMinutes: number) {
+	const positions = BODY_POSITIONS.get(code)!
+
+	let startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 12, 0, 0, 0)
+	if (date.getHours() < 12) startTime = new Date(startTime.getTime() - 86400000)
+	const time = Math.trunc(startTime.getTime() / 1000)
+	const altitude: number[] = []
+
+	altitude.push(positions.get(time)!.altitude)
+
+	for (let i = stepSizeInMinutes; i <= 1440 - stepSizeInMinutes; i += stepSizeInMinutes) {
+		altitude.push(positions.get(time + i * 60)!.altitude)
+	}
+
+	altitude.push(positions.get(time + 1440 * 60)!.altitude)
+
+	return altitude
+}
+
+function makeBodyPositionFromEphemeris(ephemeris: ObserverTable): readonly [number, BodyPosition][] {
+	return ephemeris.data.map((e) => {
+		const lightTime = parseFloat(e[9]) || 0
+		let distance = lightTime * (SPEED_OF_LIGHT * 0.06) // km
+		let distanceUnit = 'km'
+
+		if (distance <= 0) {
+			distance = 0
+		} else if (distance >= AU_KM) {
+			distance /= AU_KM
+			distanceUnit = 'AU'
+		}
+
+		return [
+			Math.trunc(new Date(`${e[0]}Z`).getTime() / 1000),
+			{
+				rightAscensionJ2000: parseAngle(e[1]),
+				declinationJ2000: parseAngle(e[2]),
+				rightAscension: parseAngle(e[3]),
+				declination: parseAngle(e[4]),
+				azimuth: parseAngle(e[5]),
+				altitude: parseAngle(e[6]),
+				magnitude: parseFloat(e[7]),
+				constellation: e[13].toUpperCase(),
+				distance,
+				distanceUnit,
+				illuminated: parseFloat(e[10]),
+				elongation: parseAngle(e[11]),
+				leading: e[12] === '/L',
+			} as BodyPosition,
+		]
+	})
 }
